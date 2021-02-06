359aec147e7ae7b3470a811959110bc942698e79
Tahmina <thm.rasoli@gmail.com>
2021-02-06 12:28:33 +0100

    Update timer.js

--- src/handlers/timer.js
+++ src/handlers/timer.js
@@ -1,9 +1,7 @@
 
 import {displayTimeLeft} from '../handlers/displayTimeLeft.js'
 import {displayEndTime} from '../handlers/displayEndTime.js'
-// const timerDisplay = document.querySelector('.display__time-left');
-// const endTime = document.querySelector('.display__end-time');
-// const buttons = document.querySelectorAll('[data-time]');
+
 let countdown;
 export function timer(seconds) {
   // clear any existing timers
@@ -26,32 +24,11 @@ export function timer(seconds) {
   }, 1000);
 }
 
-// function displayTimeLeft(seconds) {
-//   const minutes = Math.floor(seconds / 60);
-//   const remainderSeconds = seconds % 60;
-//   const display = `${minutes}:${remainderSeconds < 10 ? '0' : '' }${remainderSeconds}`;
-//   document.title = display;
-//   timerDisplay.textContent = display;
-// }
 
-// function displayEndTime(timestamp) {
-//   const end = new Date(timestamp);
-//   const hour = end.getHours();
-//   const adjustedHour = hour > 12 ? hour - 12 : hour;
-//   const minutes = end.getMinutes();
-//   endTime.textContent = `Be Back At ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`;
-// }
 
 export function startTimer() {
   const seconds = parseInt(this.dataset.time);
   timer(seconds);
 }
 
-// buttons.forEach(button => button.addEventListener('click', startTimer));
-// document.customForm.addEventListener('submit', function(e) {
-//   e.preventDefault();
-//   const mins = this.minutes.value;
-//   console.log(mins);
-//   timer(mins * 60);
-//   this.reset();
-// });
+
